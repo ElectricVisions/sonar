0be82b86d03f95a001c556249e9c2b36e21ffe1c
Phil Thompson <phil@electricvisions.com>
2017-05-25 20:08:33 +0100

    display all data with some minimal styling

--- index.android.js
+++ index.android.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, FlatList } from 'react-native';
+import { AppRegistry, View, Text, SectionList } from 'react-native';
 import guideData from './guide.json';
 
 class SonarApp extends Component {
   render() {
     const data = guideData.map( (section) => {
-      return section.artists
-    } ).reduce( (total, current) => total.concat(current), []
-    ).map( (artist) => ({key: artist.name}) )
+      return {
+        data: section.artists,
+        key: `${section.day} ${section.venue}`
+      };
+    } );
     return (
-      <FlatList
-        data={data}
-        renderItem={({item}) => <Text>{item.key}</Text>}
+      <SectionList
+        renderItem={({item}) =>
+            <View style={{paddingBottom: 10, paddingLeft: 10}}>
+              <Text style={{fontWeight: "bold"}}>{item.name}</Text>
+              <Text>{item.style}</Text>
+              <Text>{item.description}</Text>
+            </View>
+        }
+        renderSectionHeader={({section}) =>
+            <Text style={{
+              padding: 10,
+              fontWeight: "bold",
+              fontSize: 22}}>{section.key}</Text>}
+        sections={data}
       />
     );
   }
