6aa5a4b8e75da0b86656d1502bcc3e8b35d4c0e6
Phil Thompson <phil@electricvisions.com>
2017-05-28 07:39:37 +0100

    Use ScrollView as SectionList scrolling not yet released
    * Also renamed a few keys in guide.json

--- js/guide_view.js
+++ js/guide_view.js
@@ -1,38 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text, SectionList } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import guideData from '../guide.json';
 import { ArtistView } from './artist_view';
 import { HeaderView } from './header_view';
 
 export class GuideView extends Component {
   render() {
-    const data = guideData.map( (section) => {
-      return {
-        data: section.artists.map( (artist) => ({
-          key: `${artist.location}${section.day}${artist.time}`,
-          name: artist.name,
-          genre: artist.style,
-          comment: artist.description,
-          location: artist.location,
-          type: artist.type,
-          from: artist.from,
-          time: artist.time
-        }) ),
-        key: `${section.day} ${section.venue}`
-      };
-    } );
+    const data = guideData.map( (section) =>
+      <View key={`${section.day}${section.venue}`}>
+        <HeaderView title={section.day}/>
+        {section.artists.map( (artist) =>
+          <ArtistView
+            key={`${artist.location}${section.day}${artist.time}`}
+            artist={artist}
+          />
+        )}
+      </View>
+    );
 
     return (
-      <SectionList
-        style={{backgroundColor: 'rgb(233, 211, 218)'}}
-        renderItem={({item}) =>
-            <ArtistView artist={item}/>
-        }
-        renderSectionHeader={({section}) =>
-            <HeaderView title={section.key}/>
-        }
-        sections={data}
-      />
+      <ScrollView
+        style={{backgroundColor: 'rgb(233, 211, 218)'}}>
+        {data}
+      </ScrollView>
     );
   }
 }
